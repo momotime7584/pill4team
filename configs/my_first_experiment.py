a10bf1377ba7117bad814c58aaf68c9abc32f4ac
kjh <jehakim2210@gmail.com>
2025-07-30 11:37:38 +0900

    chore: change import path

--- configs/my_first_experiment.py
+++ configs/my_first_experiment.py
@@ -1,5 +1,5 @@
 # 1. 부품들을 import
-from ._base_.models.faster_rcnn_r50 import model
+from ._base_.models.faster_rcnn_r50_fpn import model
 from ._base_.datasets.pill_detection import data
 from ._base_.schedules.sgd_20ep import optimizer, lr_scheduler
 from ._base_.default_runtime import seed, device, callbacks, mlflow, training
